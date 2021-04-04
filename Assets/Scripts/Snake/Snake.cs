549f26efc053475b492414a8461264092fe24ed8
GostyuninVA <48084264+GostyuninVA@users.noreply.github.com>
2021-04-05 00:33:10 +0300

    Init

---
+++ Assets/Scripts/Snake/Snake.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(TailGeneration))]
+[RequireComponent(typeof(SnakeInput))]
+public class Snake : MonoBehaviour
+{
+    [SerializeField] private SnakeHead _head;
+    [SerializeField] private int _tailSize;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _tailSpringness;
+
+    private SnakeInput _input;
+    private List<Segment> _tail;
+    private TailGeneration _tailGeneration;
+
+    public UnityAction<int> SizeUpdate;
+
+    private void Awake()
+    {
+        _tailGeneration = GetComponent<TailGeneration>();
+        _input = GetComponent<SnakeInput>();
+
+        _tail = _tailGeneration.Generate(_tailSize);
+
+        SizeUpdate?.Invoke(_tail.Count);
+    }
+
+    private void OnEnable()
+    {
+        _head.BlockCollided += OnBlockCollided;
+        _head.BonusCollected += OnBonusColleted;
+    }
+
+    private void OnDisable()
+    {
+        _head.BlockCollided -= OnBlockCollided;
+        _head.BonusCollected -= OnBonusColleted;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move(_head.transform.position + _head.transform.up * _speed * Time.fixedDeltaTime);
+
+        _head.transform.up = _input.GetDirectionToClick(_head.transform.position);
+    }
+
+    private void Move(Vector2 nextPosition)
+    {
+        Vector2 previousPosition = _head.transform.position;
+
+        foreach(var segment in _tail)
+        {
+            Vector2 tempPosition = segment.transform.position;
+            segment.transform.position = Vector2.Lerp(segment.transform.position, previousPosition, _tailSpringness * Time.fixedDeltaTime);
+            previousPosition = tempPosition;
+        }
+
+        _head.Move(nextPosition);
+    }
+
+    private void OnBlockCollided()
+    {
+        Segment deletedSegment = _tail[_tail.Count - 1];
+        _tail.Remove(deletedSegment);
+        Destroy(deletedSegment.gameObject);
+        SizeUpdate?.Invoke(_tail.Count);
+    }
+
+    private void OnBonusColleted(int bonusSize)
+    {
+        _tail.AddRange(_tailGeneration.Generate(bonusSize));
+        SizeUpdate?.Invoke(_tail.Count);
+    }
+}
---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _container;
+    [SerializeField] private int _repeatCount;
+    [SerializeField] private int _distanceBetweenFullLine;
+    [SerializeField] private int _distanceBetweenRandomLine;
+
+    [SerializeField] private Block _blockTempLate;
+    [SerializeField] private int _blockSpawnChance;
+
+    private SpawnPoint[] _blockSpawnPoints;
+
+    private void Start()
+    {
+        _blockSpawnPoints = GetComponentsInChildren<SpawnPoint>();
+
+        for(int i = 0; i < _repeatCount; i++)
+        {
+            GenerateFullLine(_blockSpawnPoints, _blockTempLate.gameObject);
+            MoveSpawner(_distanceBetweenFullLine);
+        }
+    }
+
+    private void GenerateFullLine(SpawnPoint[] spawnPoints, GameObject generatedElement)
+    {
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            GenerateElement(spawnPoints[i].transform.position, generatedElement);
+        }
+    }
+
+    private void GenerateRandomElements()
+    {
+
+    }
+    
+    private GameObject GenerateElement(Vector3 spawnPoint, GameObject generatedElement)
+    {
+        spawnPoint.y -= generatedElement.transform.localScale.y;
+        return Instantiate(generatedElement, spawnPoint, Quaternion.identity, _container);
+    }
+
+    private void MoveSpawner(int distnaceY)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + distnaceY, transform.position.z);
+    }
+
+}
